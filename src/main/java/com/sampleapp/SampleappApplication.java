3909eae498618b7e6a4a12ec2340c0ec78152d9d
dhanyesh <dhanyeshkrishnan@gmail.com>
2024-10-22 17:54:12 +0530

    Update SampleappApplication.java

--- src/main/java/com/sampleapp/SampleappApplication.java
+++ src/main/java/com/sampleapp/SampleappApplication.java
@@ -21,11 +21,3 @@ public class SampleappApplication {
 	}
 
 }
-
-echo "# sampleapp" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/dhanyesh/sampleapp.git
-git push -u origin main
